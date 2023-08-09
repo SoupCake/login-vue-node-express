dec44e4cec9a57bd2eb25e3b8bd730ab45af1378
Steven Chow <stevenchow33@hotmail.com>
2023-08-10 01:13:54 +0800

    setting up registration page

--- server/src/app.js
+++ server/src/app.js
@@ -8,9 +8,10 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: "Hello World"
+        message: `Hello ${req.body.email}! You have been registered!`,
+
     })
 });
 app.listen(process.env.PORT || 8081);
